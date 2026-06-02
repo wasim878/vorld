6cd9b2e9b77f9c3cfec505bd75d37a16b775dcb7
mrwaseem <mrwaseemmm@gmail.com>
2026-06-03 01:28:36 +0530

    new scene

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert("hey");
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const canvas = document.querySelector("#draw")
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+function animate( time ) {
+    window.requestAnimationFrame(animate);
+  renderer.render( scene, camera );
+  cube.rotation.x = time / 2000;
+cube.rotation.y = time / 1000;
+}
